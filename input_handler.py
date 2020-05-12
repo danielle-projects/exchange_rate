dbcdcb3304430813d7dc7c9219b459dee93e75fc
danielle <danielle@trellis.ag>
2020-05-12 21:40:53 +0300

    create

--- input_handler.py
+++ input_handler.py
@@ -1,32 +1,23 @@
 import pandas as pd
 
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+from datetime import datetime
+
+from dateutil import relativedelta
 
 from exchange_rate_consts import ExchangeRateConsts
 
 
 class InputHandler:
-    def __init__(self, currency, start_date, end_date):
+    def __init__(self, currency, graph_formatter):
         self.input_file_path = ExchangeRateConsts.EXCHANGE_RATE_INPUT_FILE
         self.currency = currency
-        self.start_date = start_date
-        self.end_date = end_date
+        # self.start_date = pd.to_datetime(start_date)
+        # self.end_date = pd.to_datetime(end_date)
+        self.graph_formatter = graph_formatter
+        self.currency_rate_df = self.convert_input_file_to_df()
 
     def convert_input_file_to_df(self):
         exchange_rate_df = pd.read_excel(self.input_file_path)
-        exchange_rate_df.sort_values(by='DATE', ascending=False, inplace=True)
+        return exchange_rate_df
 
-        exchange_rate_df = exchange_rate_df.head(8)
-        ax = exchange_rate_df.plot(kind='line', x='DATE', y='US DOLLAR', color='blue', title='Currency Rate',
-                                   figsize=(15, 10))
-        plt.ylabel("Rate")
-        plt.xlabel("Date")
-        plt.legend(['Date, Currency rate'])
-        ax.xaxis.set_major_locator(mdates.DayLocator())
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%Y'))
-        plt.show()
-        print(exchange_rate_df)
 
-    def slice_df_by_date_range(self, df):
-        pass
